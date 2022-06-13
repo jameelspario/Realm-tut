f61e1cc7972336d1e1999e59e7de3b516ded8ab0
spario <jameelspario@gmail.com>
2022-06-13 13:44:24 +0530

    Realm db

--- app/src/main/java/com/example/realmtut/MainActivity.java
+++ app/src/main/java/com/example/realmtut/MainActivity.java
@@ -3,12 +3,76 @@ package com.example.realmtut;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Realm realm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        realm = Realm.getDefaultInstance();
+
+
+    }
+
+
+    private void addDataToDatabase(String courseName, String courseDescription, String courseDuration, String courseTracks) {
+
+        // on below line we are creating
+        // a variable for our modal class.
+        DataModal modal = new DataModal();
+
+        // on below line we are getting id for the course which we are storing.
+        Number id = realm.where(DataModal.class).max("id");
+
+        // on below line we are
+        // creating a variable for our id.
+        long nextId;
+
+        // validating if id is null or not.
+        if (id == null) {
+            // if id is null
+            // we are passing it as 1.
+            nextId = 1;
+        } else {
+            // if id is not null then
+            // we are incrementing it by 1
+            nextId = id.intValue() + 1;
+        }
+        // on below line we are setting the
+        // data entered by user in our modal class.
+        modal.setId(nextId);
+        modal.setCourseDescription(courseDescription);
+        modal.setCourseName(courseName);
+        modal.setCourseDuration(courseDuration);
+        modal.setCourseTracks(courseTracks);
+
+        // on below line we are calling a method to execute a transaction.
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                // inside on execute method we are calling a method
+                // to copy to real m database from our modal class.
+                realm.copyToRealm(modal);
+            }
+        });
+    }
+
+    public void update(View view) {
+        addDataToDatabase(
+                "enlish",
+                "english speaking practice",
+                "1 hour",
+                "asd"
+
+        );
     }
 }
+
